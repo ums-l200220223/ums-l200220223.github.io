277a560a544f346e47a46aa4c0841be5efc49aa9
bayu <l200220223@student.ums.ac.id>
2024-12-29 23:33:29 +0700

    rev

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ def index():
 @app.route('/run_metaflow', methods=['POST'])
 def run_metaflow():
     try:
-        # Execute the Metaflow code
+        # Run the Metaflow script
         result = subprocess.run(
-            ['python3', 'kuliah_flow.py'], capture_output=True, text=True
+            ['python3', 'ilustrasikuliah.py'], capture_output=True, text=True
         )
         return jsonify({"output": result.stdout, "error": result.stderr})
     except Exception as e:
